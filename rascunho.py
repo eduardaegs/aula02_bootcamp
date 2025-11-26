0c3b1760f935551c2b1035798ac4541e6576349c
Eduarda Andrade <andrasi.eduarda@gmail.com>
2025-11-26 08:08:03 -0300

    Exercício com Int

---
+++ rascunho.py
@@ -0,0 +1,15 @@
+print(6 % 4) # usado para pegar o resto inteiro da divisao
+print(6 // 4) # usado para pegar a parte inteira da divisao
+print(2 ** 3) # usado para potenciacao
+print(3 + 2 * 5) # ordem de precedencia
+print((3 + 2) * 5) # uso de parenteses para alterar a ordem de precedencia
+print(1 + 1) # adicao
+print(3 - 1) # subtracao
+print(4 * 2) # multiplicacao
+print(8 / 2) # divisao
+
+nome_aluno = 'Eduarda'
+
+print(type(nome_aluno)) # verificando o tipo da variavel
+
+print(nome_aluno.upper()) # deixando todas as letras maiusculas
