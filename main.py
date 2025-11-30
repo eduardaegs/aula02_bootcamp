3d3a6a117d615cb5e842f31a0de5fb0fbbc351f3
Eduarda Andrade <andrasi.eduarda@gmail.com>
2025-11-30 09:46:08 -0300

    Adicionando arquivo main.py

---
+++ main.py
@@ -0,0 +1,27 @@
+# try:
+#     numero_01 = input('Digite um número: ')
+#     resultado = numero_01 / 0
+#     print(resultado)
+# except TypeError as e:
+#     print(e) # o código do erro pode entrar num log de erros para ser mapeado posteriormente
+# else:
+#     print('Código executado sem erros')
+# finally:
+#     print('Execução finalizada')  
+
+
+
+# numero = int(input('insira um número: '))
+# if isinstance(numero, int):
+#     print('A variável é um número inteiro')
+# else:
+#     print('A variável não é um número inteiro')
+
+idade = 27
+
+if idade < 18:
+    print('Não pode dirigir')
+else:
+    print('Pode dirigir')
+
+
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,5 @@
-# Exercício 20
-numero1 = int(input('Digite o primeiro número: '))
-numero2 = int(input('Digite o segundo número: '))
-resultado_num = numero1 != numero2
-print(f'Os valores digitados são diferentes? {resultado_num}')
+numero = input('insira um número: ')
+if isinstance(numero, bool):
+    print('A variável é um número inteiro')
+else:
+    print('A variável não é um número inteiro')
