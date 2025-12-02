11b8a3a312bed5d02ac001097544057c45eeb2c5
Eduarda Andrade <andrasi.eduarda@gmail.com>
2025-12-01 22:25:05 -0300

    Salvando ajustes no código

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,10 @@
-numero = input('insira um número: ')
-if isinstance(numero, bool):
-    print('A variável é um número inteiro')
+try:
+    numero_01 = input('Digite um número: ')
+    resultado = numero_01 / 0
+    print(resultado)
+except TypeError as e:
+    print(e) # o código do erro pode entrar num log de erros para ser mapeado posteriormente
 else:
-    print('A variável não é um número inteiro')
+    print('Código executado sem erros')
+finally:
+    print('Execução finalizada')  
