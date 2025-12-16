71f6c5f5522dc77a00e23db3bbdfd7f5a6aa93f8
Eduarda Andrade <andrasi.eduarda@gmail.com>
2025-12-16 08:15:08 -0300

    Ajustes no arquivo ma

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
-# try:
-#     numero_01 = input('Digite um número: ')
-#     resultado = numero_01 / 0
-#     print(resultado)
-# except TypeError as e:
-#     print(e) # o código do erro pode entrar num log de erros para ser mapeado posteriormente
-# else:
-#     print('Código executado sem erros')
-# finally:
-#     print('Execução finalizada')  
+try:
+    numero_01 = input('Digite um número: ')
+    resultado = numero_01 / 0
+    print(resultado)
+except TypeError as e:
+    print(e) # o código do erro pode entrar num log de erros para ser mapeado posteriormente
+else:
+    print('Código executado sem erros')
+finally:
+    print('Execução finalizada')  
 
 
 
-# numero = int(input('insira um número: '))
-# if isinstance(numero, int):
-#     print('A variável é um número inteiro')
-# else:
-#     print('A variável não é um número inteiro')
+numero = int(input('insira um número: '))
+if isinstance(numero, int):
+    print('A variável é um número inteiro')
+else:
+    print('A variável não é um número inteiro')
 
 idade = 27
 
